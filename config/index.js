f0beec6837f2cdd645d865808bc96ac172376a95
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2021-10-28 19:36:30 +0200

    middleware for authentication

---
+++ config/index.js
@@ -0,0 +1,61 @@
+// We reuse this import in order to have access to the `body` property in requests
+const express = require("express");
+
+// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// https://www.npmjs.com/package/morgan
+const logger = require("morgan");
+
+// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
+
+// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
+// unless the request if from the same domain, by default express wont accept POST requests
+const cors = require("cors");
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+// Middleware configuration
+module.exports = (app) => {
+  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+  // Services like heroku use something called a proxy and you need to add this to your server
+  app.set("trust proxy", 1);
+
+  // controls a very specific header to pass headers from the frontend
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.ORIGIN || "http://localhost:3000",
+    })
+  );
+
+  // In development environment the app logs
+  app.use(logger("dev"));
+
+  // To have access to `body` property in the request
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "another super secret key",
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        sameSite: "none",
+        secure: process.env.NODE_ENV === "production",
+      },
+    })
+  );
+
+  app.user((req, res, next) => {
+    req.user = req.session.user || null;
+    next();
+  });
+};
